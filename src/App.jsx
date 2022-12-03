86b4fe28d8bd2fd2c0bbbb3f674d3ffe8a2312d8
Samet Topal <astopaal@gmail.com>
2022-12-03 22:36:27 +0300

    sayılar artık tek tek giriliyor, enter ile state'e kaydediliyor. son gündeyse ancak kar durumu varsa satış yapmama hatası düzeltildi. uygulama tüm detaylarıyla çalışır durumda

--- src/App.jsx
+++ src/App.jsx
@@ -5,34 +5,26 @@ function App() {
   const [prices, setPrices] = useState([]); //fiyat listesi
   //const [balance, setBalance] = useState(0); //bakiye
   // const [activeTransaction, setActiveTransaction] = useState(false)
-  let balance = 0
+  let balance = 0;
   let activeTransaction = false;
-  let transactionBalance = 0
-  let maxPurchase = 0
+  let transactionBalance = 0;
+  let maxPurchase = 0;
 
   const buy = (price) => {
     //alış
-    balance = (balance - price);
+    balance = balance - price;
   };
 
   const sell = (price) => {
     //satış
-    balance = (balance + price);
+    balance = balance + price;
   };
-
-  const handleClick = () => {
-    setPrices([
-      ...prices,
-      Number(document.getElementById("prices-input").value),
-    ]),
-      (document.getElementById("prices-input").value = "");
-  };
-
-
 
   //hesaplama fonksiyonu bir sonraki günü mevcut günle karşılaştırır.
   //fiyat artış veya azalış durumuna göre bir sonraki güne geçer veya mevut günde alım veya satım yapar
   const calculate = (prices) => {
+    console.log("FİYATLAR : ", prices);
+
     for (let i = 0; i < prices.length - 1; i++) {
       console.log("mevcut fiyat : ", prices[i]);
       console.log("yarınki fiyat : ", prices[i + 1]);
@@ -42,41 +34,50 @@ function App() {
           console.log(
             "yarın bugünden pahalı ve aktif işlem olmadığı için satın alım yapılıyor"
           );
-          if (balance>0) {
+          if (balance > 0) {
             maxPurchase = Math.floor(balance / prices[i]);
             buy(prices[i] * maxPurchase);
-            transactionBalance = prices[i] * maxPurchase
-          }
-          else{
+            transactionBalance = prices[i] * maxPurchase;
+          } else {
             buy(prices[i]);
           }
           console.log("satın alım yapıldı mevcut bakiye : ", balance);
           activeTransaction = true;
-          
         } else {
-          console.log(
-            "aktif işlem var sonraki gün bekleniyor, mevcut bakiye : ",
-            balance
-          );
-          continue;
+          if (i+1 !== prices.length-1) {
+            console.log(
+              "aktif işlem var sonraki gün bekleniyor, mevcut bakiye : ",
+              balance
+            );
+            continue;
+          } else {
+            console.log(
+              "son gün olduğu için satış yapılıyor, şu an bakiye : ",
+              balance
+            );
+            if (maxPurchase > 0) {
+              sell(prices[i+1] * maxPurchase);
+            } else {
+              sell(prices[i+1]);
+            }
+            console.log('satış işlemi tamamlandı')
+          }
         }
       } else {
         if (activeTransaction) {
           console.log(
-            "bugün yarından pahalı, fiyat düşecek, satım yapılıyor bakiyeniz : ",
+            "bugün yarından pahalı, fiyat düşecek, satış yapılıyor bakiyeniz : ",
             balance
           );
-          if (maxPurchase>0) {
-          sell(prices[i] * maxPurchase);
-
-          }
-          else {
-            sell(prices[i])
+          if (maxPurchase > 0) {
+            sell(prices[i] * maxPurchase);
+          } else {
+            sell(prices[i]);
           }
           console.log("satış işlemi tamamlandı, bakiye : ", balance);
-          activeTransaction = false
-          maxPurchase = 0
-          transactionBalance = 0
+          activeTransaction = false;
+          maxPurchase = 0;
+          transactionBalance = 0;
         } else {
           console.log(
             "bugün yarından pahalı, ancak işlem yok, daha uygun fiyattan alım için yarın bekleniyor..."
@@ -85,7 +86,7 @@ function App() {
         }
       }
     }
-    console.log("Maximum kar : ", balance+transactionBalance)
+    console.log("Maximum kar : ", balance + transactionBalance);
   };
 
   return (
@@ -94,24 +95,24 @@ function App() {
         style={{
           padding: 10,
           marginRight: 20,
+          minWidth: 300,
         }}
         id="prices-input"
         autoFocus
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            setPrices([...prices, Number(e.target.value)]);
+            e.target.autoFocus;
+            e.target.value = "";
+            console.log(prices);
+          }
+        }}
         className="prices-input"
-        placeholder="Fiyatları tek tek girin..."
+        placeholder="Fiyatları sırayla girin ve ENTER'a basın..."
       />
-
-      <button 
-      className="btn-calculate" 
-      onClick={() => {
-        handleClick()
-      }}>
-        EKLE
-      </button>
       <button
         className="btn-calculate"
         onClick={() => {
-          console.log("FİYATLAR : ", prices);
           calculate(prices);
         }}
       >
